f420a98e49e4bee39baa8394db1ab5aa9b98419b
Birdknothing <1213228277@qq.com>
2019-03-31 17:34:15 +0800

    正向后向断言的区别

--- 3-29@RegExp/find.js
+++ 3-29@RegExp/find.js
@@ -1,11 +1,11 @@
-var str = `<div><img src='http://124.234.32:9090?12&1'   `//>'http:dsd.jpg'<img src="http://123.34.png"></div>`
+var str = `<div><img src='http://124.234.32:9090?12&1'   >'http:dsd.jpg'<img src="http://123.34.png"></div>`
 /**
  * (?!)和(?=)都不占宽度，这意味着后面的
  * \1也不会将其算进去，不过里面如果有其他()也会算一个位置
  * string.match有g参数则返回数组，匹配到的元素依次排列
  * string.match无g参数则返回arr,arr[0]为匹配到的元素，arr[1]
  */
-var e = /(?! src=)(['"])[^'"]*\1(?=[\s]*)/
+var e = /(?<=src=)(['"])[^'"]*\1(?=[\s]*)/g
 var f = /src=['"]([^'"])*['"]/g
 console.log(str.match(e))
 // var arr = str.match(f)
@@ -14,7 +14,12 @@ console.log('-------------------')
 console.log(e.exec(str))
 console.log(e.exec(str))
 var reg = /\?from=(\w+)(\#)=end/;
-var ss = 'http://www.baidu.com?from=monvhh=endbalabla#'
+var ss = 'http://www.baidu.com?from=monvhh#=endbalabla#'
 var result = ss.match(reg);
 var whatiwant = result[1];//monvhh
 console.log(result)
+var str1 = 'this is a dog,that is a cat'
+var str2 = 'this is a dog,thaat is a cat'
+var ee = /.*(?<=this).*(?!that).*/g
+console.log(ee.exec(str1))
+console.log(ee.exec(str2))
